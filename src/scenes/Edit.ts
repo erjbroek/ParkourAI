1a86a6cb6373b53d1449421005034c3357e1513e
erjbroek <broe0136@hz.nl>
2024-09-09 11:43:18 +0200

    updates the basic ui

--- src/scenes/Edit.ts
+++ src/scenes/Edit.ts
@@ -14,10 +14,11 @@ export default class Edit {
   }
 
   public render(canvas: HTMLCanvasElement) {
-    GUI.fillRectangle(canvas, 0, canvas.height * 0.78, canvas.width, canvas.height * 0.22, 255, 255, 255, 0.5, 5);
-    GUI.fillRectangle(canvas, canvas.width * 0.85, canvas.height * 0.72, canvas.width * 0.15, canvas.height * 0.06, 255, 255, 255, 0.5);
+    GUI.fillRectangle(canvas, canvas.width * 0.89, canvas.height * 0.72, canvas.width * 0.15, canvas.height * 0.06, 255, 255, 255, 0.5);
+    GUI.fillRectangle(canvas, 0, canvas.height * 0.78, canvas.width, canvas.height * 0.24, 255, 255, 255, 0.5, 5);
+    GUI.fillRectangle(canvas, 0, canvas.height * 0.98, canvas.width, canvas.height * 0.02, 255, 255, 255, 0.8)
     for (let i = 0; i < 4; i++) {
-      GUI.fillRectangle(canvas, canvas.width * 0.01 + (canvas.width * 0.11 * i), canvas.height * 0.8, canvas.width * 0.1, canvas.height * 0.18, 255, 255, 255, 1, 5)
+      GUI.fillRectangle(canvas, canvas.width * 0.02 + (canvas.width * 0.1 * i), canvas.height * 0.8, canvas.width * 0.09, canvas.height * 0.16, 255, 255, 255, 1, 15)
     }
   }
 }
