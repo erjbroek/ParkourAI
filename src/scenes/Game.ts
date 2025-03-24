8a7a01970c4eb3c03fe05c7f1f0b453de9dd29b0
erjbroek <broe0136@hz.nl>
2025-03-24 22:35:54 +0100

    re-added generation number text in ui

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -332,8 +332,9 @@ export default class Game extends Scene {
     } else {
       GUI.fillRectangle(canvas, canvas.width * 0.9, canvas.height * 0.04, canvas.width * 0.08, canvas.height * 0.05, 255, 255, 255, 0.7, 10);
     }
+    GUI.writeText(canvas, `Generation: ${Game.neat.neat.generation}`, canvas.width * 0.05 - this.statistics.visualisationPosition, canvas.height * 0.065, 'center', 'system-ui', 20, 'white');
+    GUI.writeText(canvas, `Level: ${Parkour.activeLevel + 1}`, canvas.width * 0.133 - this.statistics.visualisationPosition, canvas.height * 0.065, 'center', 'system-ui', 20, 'white');
     GUI.writeText(canvas, `Alive: ${Math.round(Game.alivePlayers.length / Game.neat.players.length * 1000) / 10}%`, canvas.width * 0.21 - this.statistics.visualisationPosition, canvas.height * 0.065, 'center', 'system-ui', 20, 'white');
-    GUI.writeText(canvas, `Level: ${Parkour.activeLevel}`, canvas.width * 0.133 - this.statistics.visualisationPosition, canvas.height * 0.065, 'center', 'system-ui', 20, 'white');
     GUI.fillRectangle(canvas, canvas.width * 0.26 - this.statistics.visualisationPosition, canvas.height * 0.929, canvas.width * 0.1, canvas.height * 0.05, 0, 0, 0, 0.2, 10)
 
     if (Parkour.levels[Parkour.activeLevel].finished) {
@@ -358,7 +359,6 @@ export default class Game extends Scene {
       GUI.fillCircle(canvas, canvas.width * 0.2661 - this.statistics.visualisationPosition, canvas.height * 0.863, canvas.height * 0.008, 0, 0, 0, 0.8)
     }
 
-    // GUI.writeText(canvas, `Generation: ${Game.neat.neat.generation}`, canvas.width * 0.5, canvas.height * 0.07, 'center', 'system-ui', 14, 'black');
     // GUI.writeText(canvas, `Color mode ${Game.colorMode.toString()}`, canvas.width * 0.5, canvas.height * 0.07, 'center', 'system-ui', 14, 'black');
     GUI.writeText(canvas, 'Edit level', canvas.width * 0.9 + canvas.width * 0.04, canvas.height * 0.05 + canvas.height * 0.022, 'center', 'system-ui', 20, 'black')
     if (this.openEditor) { 
