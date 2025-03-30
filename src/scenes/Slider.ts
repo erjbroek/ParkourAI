422a1c12d4e79ef6292fae1243ead284f04c3098
erjbroek <broe0136@hz.nl>
2025-03-30 21:12:38 +0200

    finished input processing for sliders from settings

--- src/scenes/Slider.ts
+++ src/scenes/Slider.ts
@@ -17,6 +17,8 @@ export default class Slider {
 
   private width: number;
 
+  private holding: boolean = false;
+
   public constructor(minValue: number, maxValue: number, activeValue: number, posX: number, posY: number, width: number) {
     this.minValue = minValue;
     this.maxValue = maxValue;
@@ -29,10 +31,19 @@ export default class Slider {
 
   public processInput() {
     if (UICollision.checkCollision(this.posX + this.width * (this.activeValue - this.minValue / (this.maxValue - this.minValue)), this.posY, window.innerWidth * 0.02, window.innerHeight * 0.03)) {
-      console.log('yippee')
       if (MouseListener.isButtonDown(0)) {
+        this.holding = true
       }
     }
+    if (MouseListener.isButtonUp(0)) {
+      this.holding = false
+    }
+    if (this.holding) {
+      this.activeValue = Math.max(this.minValue, Math.min(this.maxValue, (MouseListener.x2 - this.posX) / this.width))
+      
+    }
+
+    console.log((this.maxValue - this.minValue) * this.activeValue + this.minValue)
   }
 
   public resetToDefault() {
@@ -41,7 +52,7 @@ export default class Slider {
 
   public render(canvas: HTMLCanvasElement) {
     const sliderWidth = canvas.width * 0.02
-    console.log(this.activeValue - this.minValue / (this.maxValue - this.minValue))
+    // console.log(this.activeValue - this.minValue / (this.maxValue - this.minValue))
     GUI.fillRectangle(canvas, this.posX, this.posY, this.width + sliderWidth, canvas.height * 0.03, 200, 200, 200, 0.8, 0)
     GUI.fillRectangle(canvas, this.posX + this.width * (this.activeValue - this.minValue / (this.maxValue - this.minValue)), this.posY, sliderWidth, canvas.height * 0.03, 0, 0, 0, 1, 0)
     // GUI.drawRectangle(canvas, this.posX  + this.width * (this.activeValue - this.minValue / (this.maxValue - this.minValue)), this.posY, sliderWidth, canvas.height * 0.03, 200, 200, 255, 0.8, 3, 0)
